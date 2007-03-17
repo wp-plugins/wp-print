ce78d012ae59e3abf74aaabde3fe9e4326e64bf6
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-03-17 07:23:48 +0000

    Adding To Central WordPress Plugins DB
    git-svn-id: https://plugins.svn.wordpress.org/wp-print/tags/2.06@8374 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ print/wp-print.php
@@ -0,0 +1,108 @@
+<?php
+/*
++----------------------------------------------------------------+
+|																							|
+|	WordPress 2.0 Plugin: WP-Print 2.06										|
+|	Copyright (c) 2005 Lester "GaMerZ" Chan									|
+|																							|
+|	File Written By:																	|
+|	- Lester "GaMerZ" Chan															|
+|	- http://www.lesterchan.net													|
+|																							|
+|	File Information:																	|
+|	- Printer Friendly Page															|
+|	- wp-print.php																		|
+|																							|
++----------------------------------------------------------------+
+*/
+
+
+### Variables
+$links_text = '';
+
+### Actions
+add_action('init', 'print_content');
+
+### Filters
+add_filter('wp_title', 'print_pagetitle');
+add_filter('comments_template', 'print_template_comments');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="Robots" content="noindex" />
+<style type="text/css" media="screen, print">
+Body {
+	font-family: Verdana, Arial, Tahoma;
+	font-size: 12px;
+	color: #000000;
+}
+#Outline {
+	text-align: left;
+	width: 90%;
+	margin-left: auto; 
+	margin-right: auto;
+	padding: 10px;
+	border: 1px solid #000000;
+}
+#BlogTitle {
+	font-weight: bold;
+	font-size: 16px;
+	margin-bottom: 5px;
+}
+#BlogDate {
+	margin-top: 5px;
+	margin-bottom: 10px;	
+}
+#BlogContent {
+	padding: 10px;
+	margin-top: 10px;
+}
+HR.Divider {
+	width: 80%; 
+	height: 1px; 
+	color: #000000;
+}
+#CommentTitle {
+	font-weight: bold;
+	font-size: 16px;
+	padding-bottom: 10px;
+}
+.CommentDate {
+	margin-top: 5px;
+	margin-bottom: 10px;
+}
+.CommentContent {
+	padding: 2px 10px 10px 10px;
+}
+</style>
+</head>
+<body>
+<p align="center"><b>- <?php bloginfo('name'); ?> - <?php bloginfo('url')?> -</b></p>
+<center>
+	<div id="Outline">
+		<?php if (have_posts()): ?>
+			<?php while (have_posts()): the_post(); ?>
+					<p id="BlogTitle"><?php the_title(); ?></p>
+					<p id="BlogDate">Posted By <u><?php the_author(); ?></u> On <?php the_time(get_settings("date_format").' @ '.get_settings("time_format")); ?> In <?php print_categories('<u>', '</u>'); ?> | <u><?php print_comments_number(); ?></u></p>
+					<div id="BlogContent"><?php print_content(); ?></div>
+			<?php endwhile; ?>
+			<hr class="Divider" align="center" />
+			<?php if(print_can('comments')): ?>
+				<?php comments_template(); ?>
+			<?php endif; ?>
+			<p align="left">Article printed from <?php bloginfo('name'); ?>: <b><?php bloginfo('url'); ?></b></p>
+			<p align="left">URL to article: <b><?php the_permalink(); ?></b></p>
+			<?php if(print_can('links')): ?>
+				<p align="left"><?php print_links(); ?></p>
+			<?php endif; ?>
+			<p align="right">Click <a href="#Print" onclick="window.print(); return false;" title="Click here to print.">here</a> to print.</p>
+		<?php else: ?>
+				<p align="center">No posts matched your criteria.</p>
+		<?php endif; ?>
+	</div>
+</center>
+</body>
+</html>
