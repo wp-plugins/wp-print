21f0b4d5a8876f51b8133a39667c6553fe2ffcb4
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-31 11:44:31 +0000

    WP-Print 2.11
    git-svn-id: https://plugins.svn.wordpress.org/wp-print/tags/2.11@13480 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ print/wp-print-comments.php
@@ -0,0 +1,34 @@
+<?php
+/*
++----------------------------------------------------------------+
+|																							|
+|	WordPress 2.1 Plugin: WP-Print 2.11										|
+|	Copyright (c) 2007 Lester "GaMerZ" Chan									|
+|																							|
+|	File Written By:																	|
+|	- Lester "GaMerZ" Chan															|
+|	- http://www.lesterchan.net													|
+|																							|
+|	File Information:																	|
+|	- Printer Friendly Page For Comments										|
+|	- wp-print-comments.php														|
+|																							|
++----------------------------------------------------------------+
+*/
+?>
+<?php if($comments) : ?>
+	<?php $comment_count = 1; ?>
+	<p id="CommentTitle"><?php print_comments_number(); ?> <?php _e('To', 'wp-print'); ?> "<?php the_title(); ?>"</p>				
+	<?php foreach ($comments as $comment) : ?>					
+		<p class="CommentDate">
+			<strong>#<?php echo $comment_count; ?> <?php comment_type(); ?></strong> <?php _e('By', 'wp-print'); ?> <u><?php comment_author(); ?></u> <?php _e('On', 'wp-print'); ?> <?php comment_date(get_option("date_format").' @ '.get_option("time_format")); ?></p>
+		<div class="CommentContent">
+			<?php if ($comment->comment_approved == '0') : ?>
+				<p><em><?php _e('Your comment is awaiting moderation.', 'wp-print'); ?></em></p>
+			<?php endif; ?>
+			<?php print_comments_content(); ?>
+		</div>
+		<?php $comment_count++; ?>
+	<?php endforeach; ?>
+	<hr class="Divider" style="text-align: center;" />
+<?php endif; ?>
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== WP-Print ===
+Contributors: GamerZ
+Donate link: http://www.lesterchan.net/wordpress
+Tags: print, printer, wp-print
+Requires at least: 2.1.0
+Stable tag: 2.11
+
+Displays a printable version of your WordPress blog's post/page.
+
+== Description ==
+
+All the information (general, changelog, installation, upgrade, usage) you need about this plugin can be found here: [WP-Print Readme](http://www.lesterchan.net/wordpress/readme/wp-print.html "WP-Print Readme").
+It is the exact same readme.html is included in the zip package.
+
+== Development Blog ==
+
+[GaMerZ WordPress Plugins Development Blog](http://www.lesterchan.net/wordpress/ "GaMerZ WordPress Plugins Development Blog")
+
+== Installation ==
+
+[WP-Print Readme](http://www.lesterchan.net/wordpress/readme/wp-print.html "WP-Print Readme") (Installation Tab)
+
+== Screenshots ==
+
+[GaMerZ WordPress Plugins Screenshots](http://www.lesterchan.net/wordpress/screenshots/ "GaMerZ WordPress Plugins Screenshots")
+
+== Frequently Asked Questions ==
+
+You will need [GaMerZ WordPress Plugins Support Forums](http://forums.lesterchan.net/ "GaMerZ WordPress Plugins Support Forums")
