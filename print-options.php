d8bf9e8984a770d6eaaf95aec1e048a178a776ec
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-04 16:24:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-print/trunk@328682 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- print-options.php
+++ print-options.php
@@ -125,7 +125,7 @@ switch($mode) {
 	/* ]]> */
 </script>
 <?php if(!empty($text)) { echo '<!-- Last Action --><div id="message" class="updated fade"><p>'.$text.'</p></div>'; } ?>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=<?php echo plugin_basename(__FILE__); ?>">
+<form method="post" action="<?php echo admin_url('admin.php?page='.plugin_basename(__FILE__)); ?>">
 <div class="wrap"> 
 	<?php screen_icon(); ?>
 	<h2><?php _e('Print Options', 'wp-print'); ?></h2> 
